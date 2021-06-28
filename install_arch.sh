ae91dcfbcf887fe5f19fe21387b97322c634b071
DedSec <55799553+DedSec77@users.noreply.github.com>
2021-06-28 18:03:35 +0600

    Add files via upload

---
+++ install_arch.sh
@@ -0,0 +1,6 @@
+sudo pacman -S python3 python-pip git
+pip3 install colorama
+pip3 install bs4
+sudo chmod +x gitsearch
+sudo cp gitsearch /usr/bin
+echo "Done."
---
+++ install_debian.sh
@@ -0,0 +1,9 @@
+sudo apt install python
+sudo apt install python2
+sudo apt install git
+pip install --upgrade pip
+pip3 install colorama
+pip3 install bs4
+sudo chmod +x gitsearch
+sudo cp gitsearch /usr/bin
+echo "Done."
