a97b6a685881ae81dff270654d8582479f046dea
DedSec <55799553+DedSec77@users.noreply.github.com>
2021-07-20 06:36:13 -0500

    gitsearch

--- install_debian.sh
+++ install_debian.sh
@@ -1,7 +1,8 @@
 sudo apt install python
 sudo apt install python2
+sudo apt install python3
+sudo apt install python3-pip
 sudo apt install git
-pip install --upgrade pip
 pip3 install colorama
 pip3 install bs4
 sudo chmod +x gitsearch
